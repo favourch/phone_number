87a31bb03ceb3ef45eddacaad29db69938479a48
ukorjidechi <ukorjidechi@gmail.com>
2017-01-29 23:01:59 +0100

    improved example file

--- src/class.phone.number.php
+++ src/class.phone.number.php
@@ -1,18 +1,21 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: ukorjidechi
+ * @author Ukor Jidechi E. << http://www.ukorjidechi.com || ukorjidechi@gmail.com >> .
  * Date: 12/4/16
  * Time: 2:27 AM
  */
 
 namespace ukorJidechi;
 
-
 class validate_phone_number {
 
     /** The phone number */
     private $pn;
+
+    const version = "0.0.1";
+
+
+    private $phone_number_length;
 
 
     /** Holds the list of all network provider */
@@ -23,7 +26,7 @@ class validate_phone_number {
 
 
     /** Network provider id as key and the network provider name as value e.g. '0802' => 'AIRTEL_NG' */
-    private $np_id = array(
+    private $np_id = array  (
         '0803' => 'MTN_NG', '0806' => 'MTN_NG', '0703' => 'MTN_NG', '0706' => 'MTN_NG',
         '0813' => 'MTN_NG', '0816' => 'MTN_NG', '0810' => 'MTN_NG', '0814' => 'MTN_NG', '0903' => 'MTN_NG',
 
@@ -47,81 +50,128 @@ class validate_phone_number {
     );
 
 
-
     /** Holds the result */
     protected $result_set = array();
 
+
+
+    /**
+     * validate_phone_number constructor.
+     * @param $phone_number
+     */
     function __construct($phone_number) {
         return $this->is_phone_number($phone_number);
     }
+
+
 
     /**
      * strips all special and numeric character from the phone number
      * @param $pn
      * @return mixed
      */
-    function remove_char($pn){
+    private function remove_char($pn){
         /** removes white space and non numeric characters from the phone number */
         return preg_replace("/[\D\s]/", '', $pn);
     }
 
-    function check_format($pn){
+
+
+
+    private function get_length($pn){
         $this->pn = $this->remove_char($pn);
-        if(strlen($this->pn) === 14){
 
-            /** verify number contains country code (cc) */
-            if(preg_match("/234[0-9]{11}/", $this->pn, $matches)){
+        $this->result_set[0]['phone_number_length'] = strlen($this->pn);
+        $this->result_set[0]['clean_pn'] = $this->pn;
 
-                if(preg_match("/234[0]/", $matches[0])){
-                    $this->result_set[0]['np_id'] = substr($this->pn, 3, 4);
-                    $this->result_set[0]['phone_number'] = $matches[0];
+        return $this->result_set;
+    }
+
+
+    
+    /**
+     * @param array $pn_and_length
+     * @return array
+     */
+    private function check_format(array $pn_and_length){
+        $this->pn = $pn_and_length[0]['clean_pn'];
+        $this->phone_number_length = $pn_and_length[0]['phone_number_length'];
+
+        switch ($this->phone_number_length){
+            case 11:
+
+                if (preg_match("/^0/", $this->pn)){
+                    $this->result_set[0]['np_id'] = substr($this->pn, 0, 4);
+                    /** replace the leading zero with 234 */
+                    $this->result_set[0]['phone_number'] = '234'.substr($this->pn, 1,10);
                     $this->result_set[0]['isError'] = false;
                 }else{
                     $this->result_set[0]['isError']  = true;
+                    #todo:: remove redundant error message and code
+                    $this->result_set[0]['msg'] = "Number should begin with zero";
+                    $this->result_set[0]['code'] = "9xx";
+                }
+
+                break;
+            case 13:
+
+                if (preg_match("/234(8|7|9)/", $this->pn)){
+                    /**
+                     * number meet condition, append zero to the beginning...
+                     * example country_code-80 becomes country_code-080
+                     */
+                    $this->result_set[0]['np_id'] = '0'.substr($this->pn, 3, 3);
+                    $this->result_set[0]['phone_number'] = $this->pn;
+                    $this->result_set[0]['isError'] = false;
+                }else{
+                    /** error */
+                    $this->result_set[0]['isError']  = true;
+                    #todo:: remove redundant error message and code
                     $this->result_set[0]['msg'] = "Number should begin with zero after country code (234)";
                     $this->result_set[0]['code'] = "9xx";
                 }
-            }else{
-                $this->result_set[0]['isError']  = true;
-                $this->result_set[0]['msg'] = "Number is not a valid phone number";
-                $this->result_set[0]['code'] = "9xx";
-            }
-        }
-        elseif (strlen($this->pn) === 11){
 
-            if (preg_match("/^0/", $this->pn)){
-                $this->result_set[0]['np_id'] = substr($this->pn, 0, 4);
-                /** replace the leading zero with 234 */
-                $this->result_set[0]['phone_number'] = '234'.substr($this->pn, 1,10);
-                $this->result_set[0]['isError'] = false;
-            }else{
-                $this->result_set[0]['isError']  = true;
-                $this->result_set[0]['msg'] = "Number should begin with zero";
-                $this->result_set[0]['code'] = "9xx";
-            }
-        }
-        /** validate if number contains country code and it is preceded by either 7 0r 8 0r 9*/
-        elseif (strlen($this->pn) === 13){
+                break;
+            case 14:
 
-            if (preg_match("/234(8|7|9)/", $this->pn)){
-                /** number meet condition, append zero to the beginning */
-                $this->result_set[0]['np_id'] = '0'.substr($this->pn, 3, 3);
-                $this->result_set[0]['phone_number'] = $this->pn;
-                $this->result_set[0]['isError'] = false;
-            }else{
-                /** error */
-                $this->result_set[0]['isError']  = true;
-                $this->result_set[0]['msg'] = "Number should begin with zero after country code (234)";
-                $this->result_set[0]['code'] = "9xx";
-            }
+                /** verify number contains country code (cc) */
+                if(preg_match("/234[0-9]{11}/", $this->pn, $matches)){
+
+                    if(preg_match("/234[0]/", $matches[0])){
+                        $this->result_set[0]['np_id'] = substr($this->pn, 3, 4);
+                        $this->result_set[0]['phone_number'] = preg_replace("/^2340/", '234', $matches[0]) ;
+                        $this->result_set[0]['isError'] = false;
+                    }else{
+                        $this->result_set[0]['isError']  = true;
+                        #todo:: remove redundant error message and code
+                        $this->result_set[0]['msg'] = "Number should begin with zero after country code (234)";
+                        $this->result_set[0]['code'] = "9xx";
+                    }
+                }else{
+                    $this->result_set[0]['isError']  = true;
+                    #todo:: remove redundant error message and code
+                    $this->result_set[0]['msg'] = "Phone number is invalid";
+                    $this->result_set[0]['code'] = "9xx";
+                }
+
+                break;
+
+            default:
+                $this->result_set[0]['msg'] = "Phone number is not a valid number";
+                break;
+
         }
 
         return $this->result_set;
     }
 
-    private function is_cc_present($pn){}
 
+    /**
+     * @param $pn
+     * @return array
+     */
     private function identify_np($pn){
+        $pn = $this->get_length($pn);
         $this->pn = $this->check_format($pn);
 
         if($this->pn[0]['isError'] !== true){
@@ -130,18 +180,17 @@ class validate_phone_number {
                 if($this->pn[0]['np_id'] === $code){
                     $this->result_set[0]['code'] = 200;
                     $this->result_set[0]['msg'] = "Number is a valid number...";
-                    $this->result_set[0]['phone_number'] = $this->pn[0]['phone_number'];
+                    $this->result_set[0]['phone_number'] = $pn[0]['clean_pn'];
                     $this->result_set[0]['network_provider'] = $np;
-                    $this->result_set[0]['isError']  = false;
+                    $this->result_set[0]['isError']  = False;
 
                     //get out of the loop, if match was found...
                     break;
                 }else{
                     $this->result_set[0]['code'] = 404;
-                    $this->result_set[0]['msg'] = "Number is not a valid number...";
-                    $this->result_set[0]['phone_number'] = $this->pn[0]['phone_number'];
-                    $this->result_set[0]['network_provider'] = null;
-                    $this->result_set[0]['isError']  = true;
+                    $this->result_set[0]['phone_number'] = $pn[0]['clean_pn'];
+                    $this->result_set[0]['network_provider'] = Null;
+                    $this->result_set[0]['isError']  = True;
                 }
             }
 
@@ -153,7 +202,12 @@ class validate_phone_number {
         return $this->result_set;
     }
 
-    function is_phone_number($pn){
+
+    /**
+     * @param $pn
+     * @return array
+     */
+    public function is_phone_number($pn){
         return $this->identify_np($pn);
     }
 
